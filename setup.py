3e72eb272dc4c499511d5048035ae051f1176efe
ben.vanbavel@kuleuven.be <ben.vanbavel@kuleuven.be>
2023-03-18 21:12:56 +0100

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='HomoComPy',
+    version='1.0.1.',
+    packages=['Examples', 'homocompy'],
+    url='https://github.com/benvb-97/HomoComPy',
+    license='',
+    author='u0122906',
+    author_email='',
+    description=''
+)
